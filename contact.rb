0609a92788b7ca3f89112ae135a6dbfaf443b48b
sunnykhatter <lakshaykhatter@gmail.com>
2013-06-11 12:46:12 -0400

    Contact Initialization Complete

---
+++ contact.rb
@@ -0,0 +1,20 @@
+
+class Contact
+
+	attr_accessor :id, :firstname, :lastname, :email, :notes
+
+	def initialize(id, firstname, lastname, email, notes) 
+		@ID = id
+		@firstname = firstname
+		@lastname = lastname
+		@email = email
+		@notes = notes
+
+
+		 end
+
+
+
+
+
+	end
