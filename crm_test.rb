8bbf22cc1e4869c61dfc2d5f22a52bb3250298a3
Eric Szeto <eszeto@gmail.com>
2013-06-11 14:23:40 -0400

    test for modify setup

--- crm_test.rb
+++ crm_test.rb
@@ -8,7 +8,8 @@ class CrmTest < Test::Unit::TestCase
 		@database = Database.new
 		@contact = Contact.new(1, "Eric", "Szeto", "Email@mail", "notes")
 		@contact1 = Contact.new(2, "Lakshay", "Khatter", "gmail" , "notes")
-
+		@database.add(@contact)
+		@database.add(@contact1)
 	end
 
 	def test_is_an_instance_of_database
@@ -18,14 +19,13 @@ class CrmTest < Test::Unit::TestCase
 	def test_add_to_database
 		#knowing zero contacts are in database, we check to size to see if a contact has been 
 		#added 
-
+		@database = Database.new
 		@database.add(@contact)
-
 		assert_equal 1, @database.contact_array.size
-
 	end
 
 	def test_delete_contact
+		@database = Database.new
 		@database.add(@contact)
 		assert_equal 1, @database.contact_array.size
 		@database.delete("Eric")
@@ -34,21 +34,20 @@ class CrmTest < Test::Unit::TestCase
 
 	
 	def test_display_all_output
-		@database.add(@contact)
-		@database.add(@contact1)
 		assert_equal "1 Eric Szeto Email@mail notes\n2 Lakshay Khatter gmail notes\n",@database.display_all
 	end
 
 	def test_display_specific_contact
-		@database.add(@contact)
-		@database.add(@contact1)
 		assert_equal "2 Lakshay Khatter gmail notes\n", @database.display_contact("Lakshay")
 	end
 
 	def test_display_attributes_of_contacts
-		@database.add(@contact)
-		@database.add(@contact1)
 		assert_equal "Eric\nLakshay\n", @database.display_by_attribute("firstname")
 	end
+
+	def test_modify_attribute_with_valid_information
+		assert_equal true, @database.modify("firstname", "Eric", "Dave")
+	end
+
 
 end
